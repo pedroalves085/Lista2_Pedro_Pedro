28d962707808c955a1043f56e20906bf24f24125
pedroalves085 <pedroricardo14escola@gmail.com>
2026-04-05 09:27:21 -0300

    Finaliza implementação de lista duplamente encadeda

--- DLList.cpp
+++ DLList.cpp
@@ -35,13 +35,60 @@ void OrdersList::insert(Order value){
 }
 
 void OrdersList::removeFront(){
+    if (head == nullptr) return;
 
+    OrderNode* temp = this->head;
+    this->head = this->head->next;
+
+    if (this->head != nullptr) {
+        this->head->prev = nullptr;
+    } else {
+        this->tail = nullptr;
+    }
+
+    delete temp;
+    size--;
 }
 
 void OrdersList::removeEnd(){
+    if (tail == nullptr) return;
 
+    OrderNode* temp = this->tail;
+    this->tail = this->tail->prev;
+
+    if (this->tail != nullptr){
+        this->tail->next = nullptr;
+    } else {
+        this->head = nullptr;
+    }
+
+    delete temp;
+    size--;
 }
 
+//Decidir oq ue queremos comparar dos objetos order para o while, colocando preço por enquanto
 void OrdersList::remove(Order value){
+    OrderNode* current = this->head;
+    while (current != nullptr && current->value.getPrice() != value.getPrice()){
+        current = current->next;
+    }
     
+    if (current == nullptr) return;
+
+    if (current == head) {
+        removeFront();
+        return;
+    }
+
+    if (current == tail) {
+        removeEnd();
+        return;
+    }
+
+    current->prev->next = current->next;
+    current->next->prev = current->prev;
+
+    delete current;
+    size--;
+
 }
